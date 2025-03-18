24441c86e1e2e21df5402dba71c5ef10621629ce
Manticore Sigma <manticoresigma@googlemail.com>
2025-03-18 12:12:30 +0100

    Improved headline function to work correctly

--- utils/styleguide.js
+++ utils/styleguide.js
@@ -47,12 +47,44 @@ function boldtext(text, color) {
 }
 
 function headline(text, color) {
-    console.log(chalk.bold.color("=============================="));
-    console.log(chalk.bold.color(text));
-    console.log(chalk.bold.color("=============================="));
+    const length = text.length;
+    switch (color) {
+        case "red":
+            console.log(chalk.bold.red("=".repeat(length)));
+            console.log(chalk.bold.red(text));
+            console.log(chalk.bold.red("=".repeat(length)));
+            break;
+        case "blue":
+            console.log(chalk.bold.blue("=".repeat(length)));
+            console.log(chalk.bold.blue(text));
+            console.log(chalk.bold.blue("=".repeat(length)));
+            break;
+        case "yellow":
+            console.log(chalk.bold.yellow("=".repeat(length)));
+            console.log(chalk.bold.yellow(text));
+            console.log(chalk.bold.yellow("=".repeat(length)));
+            break;
+        case "cyan":
+            console.log(chalk.bold.cyan("=".repeat(length)));
+            console.log(chalk.bold.cyan(text));
+            console.log(chalk.bold.cyan("=".repeat(length)));
+            break;
+        case "green":
+            console.log(chalk.bold.green("=".repeat(length)));
+            console.log(chalk.bold.green(text));
+            console.log(chalk.bold.green("=".repeat(length)));
+            break;
+        default:
+            console.log(chalk.bold.white("=".repeat(length)));
+            console.log(chalk.bold.white(text));
+            console.log(chalk.bold.white("=".repeat(length)));
+            break;
+    }
+
 }
 
 module.exports = {
     colortext,
-    boldtext
+    boldtext,
+    headline
 }
